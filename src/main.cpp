6995a2e7507fe9bcc763ac32e5ba383cd0ebe945
Frank Knoll <Knoll_Frank@web.de>
2018-09-04 18:43:36 +0200

    refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -163,7 +163,7 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s,
   return {x,y};
 }
 
-std::tuple<vector<double>, vector<double>> doit2(
+std::tuple<vector<double>, vector<double>> doit(
     double &ref_vel,
     int &lane,
     vector<double> &map_waypoints_x,
@@ -333,46 +333,6 @@ std::tuple<vector<double>, vector<double>> doit2(
   return std::make_tuple(next_x_vals, next_y_vals);
 }
 
-std::tuple<vector<double>, vector<double>> doit(
-    double &ref_vel,
-    int &lane,
-    vector<double> &map_waypoints_x,
-    vector<double> &map_waypoints_y,
-    vector<double> &map_waypoints_s,
-    vector<double> &map_waypoints_dx,
-    vector<double> &map_waypoints_dy,
-    nlohmann::basic_json<std::map, std::vector,
-        std::__cxx11::basic_string<char, std::char_traits<char>,
-            std::allocator<char> >, bool, long, unsigned long, double,
-        std::allocator, nlohmann::adl_serializer> &j) {
-
-  // j[1] is the data JSON object
-
-  // Main car's localization Data
-  double car_x = j[1]["x"];
-  double car_y = j[1]["y"];
-  double car_s = j[1]["s"];
-  double car_d = j[1]["d"];
-  double car_yaw = j[1]["yaw"];
-  double car_speed = j[1]["speed"];
-
-  // Previous path data given to the Planner
-  auto previous_path_x = j[1]["previous_path_x"];
-  auto previous_path_y = j[1]["previous_path_y"];
-
-  // Previous path's end s and d values
-  double end_path_s = j[1]["end_path_s"];
-  double end_path_d = j[1]["end_path_d"];
-
-  // Sensor Fusion Data, a list of all other cars on the same side of the road.
-  auto sensor_fusion = j[1]["sensor_fusion"];
-
-  return doit2(ref_vel, lane, map_waypoints_x, map_waypoints_y, map_waypoints_s,
-               map_waypoints_dx, map_waypoints_dy, car_x, car_y, car_s, car_d,
-               car_yaw, car_speed, previous_path_x, previous_path_y, end_path_s,
-               end_path_d, sensor_fusion);
-}
-
 int main() {
   uWS::Hub h;
 
@@ -434,14 +394,31 @@ int main() {
               vector<double> next_x_vals;
               vector<double> next_y_vals;
 
-              std::tie(next_x_vals, next_y_vals) = doit(ref_vel,
-                  lane,
-                  map_waypoints_x,
-                  map_waypoints_y,
-                  map_waypoints_s,
-                  map_waypoints_dx,
-                  map_waypoints_dy,
-                  j);
+              // j[1] is the data JSON object
+
+              // Main car's localization Data
+              double car_x = j[1]["x"];
+              double car_y = j[1]["y"];
+              double car_s = j[1]["s"];
+              double car_d = j[1]["d"];
+              double car_yaw = j[1]["yaw"];
+              double car_speed = j[1]["speed"];
+
+              // Previous path data given to the Planner
+              auto previous_path_x = j[1]["previous_path_x"];
+              auto previous_path_y = j[1]["previous_path_y"];
+
+              // Previous path's end s and d values
+              double end_path_s = j[1]["end_path_s"];
+              double end_path_d = j[1]["end_path_d"];
+
+              // Sensor Fusion Data, a list of all other cars on the same side of the road.
+              auto sensor_fusion = j[1]["sensor_fusion"];
+
+              std::tie(next_x_vals, next_y_vals) = doit(ref_vel, lane, map_waypoints_x, map_waypoints_y, map_waypoints_s,
+                           map_waypoints_dx, map_waypoints_dy, car_x, car_y, car_s, car_d,
+                           car_yaw, car_speed, previous_path_x, previous_path_y, end_path_s,
+                           end_path_d, sensor_fusion);
 
               json msgJson;
               msgJson["next_x"] = next_x_vals;
