9bec6b6dbe944e508cc2fa2cfe40e0073ab6cb37
Frank Knoll <Knoll_Frank@web.de>
2018-09-06 09:44:52 +0200

    refactoring

--- src/tests.cpp
+++ src/tests.cpp
@@ -7,13 +7,12 @@ TEST(PathPlanningTest, ShouldCreatePath) {
   refPoint.vel = 0;
 
   int lane = 1;
-  MapWaypoints map_waypoints = read_map_waypoints();
   EgoCar egoCar;
   PreviousData previousData;
   vector<Vehicle> vehicles;
 
   // WHEN
-  Points path = createPath(refPoint, lane, map_waypoints, egoCar, previousData, vehicles);
+  Points path = createPath(refPoint, lane, read_map_waypoints(), egoCar, previousData, vehicles);
 
   // THEN
   ASSERT_TRUE(true);
