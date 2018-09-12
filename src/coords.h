e27775e3d9b5beb81a365036c6f6b77a64582526
Frank Knoll <Knoll_Frank@web.de>
2018-09-12 12:21:38 +0200

    refactoring

--- src/coords.h
+++ src/coords.h
@@ -77,4 +77,82 @@ Point getXY(const Frenet &pos, const MapWaypoints &map_waypoints) {
 
   return seg + Point::fromAngle(perp_heading) * pos.d;
 }
+
+int ClosestWaypoint(const Point &point, const MapWaypoints &map_waypoints) {
+
+  double closestLen = 100000;  //large number
+  int closestWaypoint = 0;
+
+  for (int i = 0; i < map_waypoints.map_waypoints.size(); i++) {
+    double dist = point.distanceTo(map_waypoints.map_waypoints[i]);
+    if (dist < closestLen) {
+      closestLen = dist;
+      closestWaypoint = i;
+    }
+  }
+
+  return closestWaypoint;
+}
+
+int NextWaypoint(const Point &point, double theta_rad,
+                 const MapWaypoints &map_waypoints) {
+
+  int closestWaypoint = ClosestWaypoint(point, map_waypoints);
+  Point map = map_waypoints.map_waypoints[closestWaypoint];
+  double heading = (map - point).getHeading();
+  double angle = fabs(theta_rad - heading);
+  angle = min(2 * pi() - angle, angle);
+
+  if (angle > pi() / 4) {
+    closestWaypoint++;
+    if (closestWaypoint == map_waypoints.map_waypoints.size()) {
+      closestWaypoint = 0;
+    }
+  }
+
+  return closestWaypoint;
+}
+
+// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+Frenet getFrenet(const Point &point, double theta_rad,
+                 const MapWaypoints &map_waypoints) {
+  const vector<Point> &maps = map_waypoints.map_waypoints;
+  int next_wp = NextWaypoint(point, theta_rad, map_waypoints);
+
+  int prev_wp;
+  prev_wp = next_wp - 1;
+  if (next_wp == 0) {
+    prev_wp = maps.size() - 1;
+  }
+
+  const Point n = maps[next_wp] - maps[prev_wp];
+  const Point x = point - maps[prev_wp];
+
+  // find the projection of x onto n
+  // TODO: warum nicht /n.len() ?
+  double proj_norm = x.scalarProd(n) / n.scalarProd(n);
+  const Point proj = n * proj_norm;
+  double frenet_d = x.distanceTo(proj);
+
+  //see if d value is positive or negative by comparing it to a center point
+
+  const Point center = Point { 1000, 2000 } - maps[prev_wp];
+  double centerToPos = center.distanceTo(x);
+  double centerToRef = center.distanceTo(proj);
+
+  if (centerToPos <= centerToRef) {
+    frenet_d *= -1;
+  }
+
+  // calculate s value
+  double frenet_s = 0;
+  for (int i = 0; i < prev_wp; i++) {
+    frenet_s += maps[i].distanceTo(maps[i + 1]);
+  }
+
+  frenet_s += proj.len();
+
+  return Frenet { frenet_s, frenet_d };
+}
+
 #endif /* COORDS_H_ */
