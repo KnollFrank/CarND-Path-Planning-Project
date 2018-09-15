65a9d6a1cae836c2ca63987374d3dc3a879ef328
Frank Knoll <Knoll_Frank@web.de>
2018-09-15 10:44:24 +0200

    adding another test case to coordsConverterTest

--- src/coordsConverterTest.cpp
+++ src/coordsConverterTest.cpp
@@ -6,6 +6,9 @@ TEST(CoordsConverterTest, should_get_frenet) {
   // GIVEN
   MapWaypoints map_waypoints;
 
+  map_waypoints.map_waypoints.push_back(Point { 0, 10 });
+  map_waypoints.map_outwards.push_back(Point { 0, 1 });
+
   map_waypoints.map_waypoints.push_back(Point { 0, 5 });
   map_waypoints.map_outwards.push_back(Point { -1, 0 });
 
@@ -18,12 +21,12 @@ TEST(CoordsConverterTest, should_get_frenet) {
   map_waypoints.map_waypoints.push_back(Point { 10, 10 });
   map_waypoints.map_outwards.push_back(Point { 0, 1 });
 
-  map_waypoints.map_waypoints.push_back(Point { 0, 10 });
-  map_waypoints.map_outwards.push_back(Point { 0, 1 });
-
   CoordsConverter coordsConverter(map_waypoints);
 
   // WHEN & THEN
-  EXPECT_EQ((Frenet { sqrt(50) + 2, -1 }), coordsConverter.getFrenet(Point { 7,
-      1 }));
+  EXPECT_EQ((Frenet { 5 + 1 / sqrt(2), 1 / sqrt(2) }),
+            coordsConverter.getFrenet(Point { 0, 4 }));
+
+  EXPECT_EQ((Frenet { 5 + sqrt(50) + 2, -1 }), coordsConverter.getFrenet(Point {
+      7, 1 }));
 }
