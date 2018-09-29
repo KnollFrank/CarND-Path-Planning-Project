feb314e5e557d4815a24cb97c77078b3d948727d
Frank Knoll <Knoll_Frank@web.de>
2018-09-29 17:08:24 +0200

    starting to implement distance() function

--- src/parametricSpline.h
+++ src/parametricSpline.h
@@ -69,7 +69,38 @@ ParametricSpline::ParametricSpline(const vector<Point>& points) {
                         ParameterizationType::chordLength, spline);
 }
 
+vector<double> polySquared(const vector<double>& a) {
+  vector<double> d(7);
+  d[0] = a[0] * a[0];
+  d[1] = 2 * a[1] * a[0];
+  d[2] = 2 * a[2] * a[0] + a[1] * a[1];
+  d[3] = 2 * a[3] * a[0] + 2 * a[2] * a[1];
+  d[4] = 2 * a[3] * a[1] + a[2] * a[2];
+  d[5] = 2 * a[3] * a[2];
+  d[6] = a[3] * a[3];
+  return d;
+}
+
+vector<double> getDistancePrime(const Point& point, const vector<double>& a,
+                                const vector<double>& b) {
+  vector<double> d = polySquared(a);
+  vector<double> e = polySquared(b);
+  vector<double> distancePrime(6);
+  distancePrime[0] = -2 * (point.x * a[1] + d[1] + point.y * b[1] + e[1]);
+  distancePrime[1] = 2
+      * (-2 * point.x * a[2] + d[2] - 2 * point.y * b[2] + e[2]);
+  distancePrime[2] = 3
+      * (-2 * point.x * a[3] + d[3] - 2 * point.y * b[3] + e[3]);
+  distancePrime[3] = 4 * (d[4] + e[4]);
+  distancePrime[4] = 5 * (d[5] + e[5]);
+  distancePrime[5] = 6 * (d[6] + e[6]);
+  return distancePrime;
+}
+
 double distance(const Point& point, const ParametricSpline& spline) {
+  vector<double> a(4);
+  vector<double> b(4);
+  vector<double> distancePrime = getDistancePrime(point, a, b);
   return 4711;
 }
 
