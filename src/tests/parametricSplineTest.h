fb99876b4a930033a7f240760681491e82c653f8
Frank Knoll <Knoll_Frank@web.de>
2018-10-01 12:18:11 +0200

    refactoring

--- src/tests/parametricSplineTest.h
+++ src/tests/parametricSplineTest.h
@@ -26,24 +26,25 @@ TEST(ParametricSplineTest, should_get_length) {
   ParametricSpline spline(mapWaypoints.map_waypoints);
 
   // When
-  double length = spline.length();
+  double length = spline.getLength();
 
   // Then
   EXPECT_EQ(6947, int(length));
 }
 
-void check_distanceTo(const Frenet& frenet,
-                      const CoordsConverter& coordsConverter,
-                      const ParametricSpline& spline) {
+void checkDistanceBetweenPointAndSpline(
+    const Frenet& frenet, const ParametricSpline& spline,
+    const CoordsConverter& coordsConverter) {
 
   // Given
+  double distanceExpected = frenet.d;
   const Point& point = coordsConverter.getXY(frenet);
 
   // When
-  double distance = spline.distanceTo(point);
+  double distanceActual = spline.distanceTo(point);
 
   // Then
-  EXPECT_NEAR(frenet.d, distance, 0.001);
+  EXPECT_NEAR(distanceExpected, distanceActual, 0.001);
 }
 
 TEST(ParametricSplineTest, should_get_distanceBetweenPointAndSpline) {
@@ -52,7 +53,8 @@ TEST(ParametricSplineTest, should_get_distanceBetweenPointAndSpline) {
   CoordsConverter coordsConverter(mapWaypoints);
   ParametricSpline spline(mapWaypoints.map_waypoints);
 
-  check_distanceTo(Frenet { 10, 5 }, coordsConverter, spline);
+  // When & Then
+  checkDistanceBetweenPointAndSpline(Frenet { 10, 5 }, spline, coordsConverter);
 }
 
 TEST(ParametricSplineTest, should_get_distanceBetweenPointAndSpline2) {
@@ -61,7 +63,24 @@ TEST(ParametricSplineTest, should_get_distanceBetweenPointAndSpline2) {
   CoordsConverter coordsConverter(mapWaypoints);
   ParametricSpline spline(mapWaypoints.map_waypoints);
 
-  check_distanceTo(Frenet { 124.834, 5 }, coordsConverter, spline);
+  // When & Then
+  checkDistanceBetweenPointAndSpline(Frenet { 124.834, 5 }, spline,
+                                     coordsConverter);
+}
+
+TEST(ParametricSplineTest, should_get_distanceBetweenPointAndSpline3) {
+  // Given
+  MapWaypoints mapWaypoints = MapWaypoints::load();
+  CoordsConverter coordsConverter(mapWaypoints);
+  ParametricSpline spline(mapWaypoints.map_waypoints);
+  double splineLength = spline.getLength();
+
+  // When & Then
+  for (int i = 0; i < 100; i++) {
+    double s = i / 100.0 * splineLength;
+    checkDistanceBetweenPointAndSpline(Frenet { s, 5 }, spline,
+                                       coordsConverter);
+  }
 }
 
 #endif /* TESTS_PARAMETRICSPLINETEST_H_ */
