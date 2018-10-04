6389ce2fe5253db32bce8df9245872d9094d0579
Frank Knoll <Knoll_Frank@web.de>
2018-10-04 10:11:56 +0200

    refactoring

--- src/car.h
+++ src/car.h
@@ -133,6 +133,7 @@ Vehicle::Vehicle(int _id, FrenetCart _pos,
                  const CoordsConverter& _coordsConverter)
     : id(_id),
       pos(_pos),
+      vel_m_per_s(FrenetCart(Point::zero())),
       coordsConverter(_coordsConverter) {
 }
 
