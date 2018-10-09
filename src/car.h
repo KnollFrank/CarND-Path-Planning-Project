69f3be3f2fdc422fcaa6b19cfe8a08baaf8e0c10
Frank Knoll <Knoll_Frank@web.de>
2018-10-09 21:43:40 +0200

    refactoring

--- src/car.h
+++ src/car.h
@@ -9,70 +9,11 @@
 #include "coords/frenetCart.h"
 #include "dimension.h"
 #include "funs.h"
+#include "rectangle.h"
+#include <boost/circular_buffer.hpp>
+
 
 using namespace std;
-
-class Rectangle {
-
- public:
-  static Rectangle fromTopLeftAndDimension(const Frenet& topLeft,
-                                           const Dimension& dimension);
-  static Rectangle fromCenterAndDimension(const Frenet& center,
-                                          const Dimension& dimension);
-
-  Frenet getBottomRight() const {
-    return topLeft + Frenet { -dimension.getHeight(), dimension.getWidth() };
-  }
-
-  Frenet getTopLeft() const {
-    return topLeft;
-  }
-
-  double getWidth() const {
-    return dimension.getWidth();
-  }
-
-  double getHeight() const {
-    return dimension.getHeight();
-  }
-
-  bool overlaps(const Rectangle& other) const {
-    return !other.isOnLeftSideOf(*this) && !this->isOnLeftSideOf(other)
-        && !other.isAbove(*this) && !this->isAbove(other);
-  }
-
- private:
-  Rectangle(const Frenet& topLeft, const Dimension& dimension);
-
-  bool isOnLeftSideOf(const Rectangle& other) const {
-    return other.getTopLeft().d > getBottomRight().d;
-  }
-
-  bool isAbove(const Rectangle& other) const {
-    return other.getTopLeft().s < getBottomRight().s;
-  }
-
-  Frenet topLeft;
-  Dimension dimension;
-
-};
-
-Rectangle Rectangle::fromTopLeftAndDimension(const Frenet& topLeft,
-                                             const Dimension& dimension) {
-  return Rectangle(topLeft, dimension);
-}
-
-Rectangle Rectangle::fromCenterAndDimension(const Frenet& center,
-                                            const Dimension& dimension) {
-  return fromTopLeftAndDimension(
-      center + Frenet { dimension.getHeight() / 2, -dimension.getWidth() / 2 },
-      dimension);
-}
-
-Rectangle::Rectangle(const Frenet& _topLeft, const Dimension& _dimension)
-    : topLeft(_topLeft),
-      dimension(_dimension) {
-}
 
 class Circle {
 
